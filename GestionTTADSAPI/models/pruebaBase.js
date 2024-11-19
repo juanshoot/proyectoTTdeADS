931e52bd5e0b020760d62c4efde3834ce75fb124
martin cortes <cortesbuendiamartin@gmail.com>
2024-11-19 14:14:38 -0600

    se cmenta codigo y se inhres carpeta para el front

--- GestionTTADSAPI/models/pruebaBase.js
+++ GestionTTADSAPI/models/pruebaBase.js
@@ -3,7 +3,7 @@ const { getConnection } = require('../models/sqlConnection.js');
 const testQuery = async () => {
     try {
         const pool = await getConnection();
-        const [rows] = await pool.query('SELECT * FROM Usuarios'); // Cambia la consulta según tus necesidades
+        const [rows] = await pool.query('SELECT * FROM Usuarios'); //? prueba de una seleciion para obtencion de datos 
         console.log(rows);
     } catch (error) {
         console.error('Error ejecutando la consulta:', error.message);
